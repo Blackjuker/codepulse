459ef2ee1dc99eeefb7fad952b1c8eee2ec4562c
Arnaud <ngabgnaarnaud2@gmail.com>
2024-09-23 03:16:45 +0200

    creation de la navbar temporaire

--- API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f212ec5d103eddca08467bef722c4dc8875f8c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
