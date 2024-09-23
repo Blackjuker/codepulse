f212ec5d103eddca08467bef722c4dc8875f8c62
Arnaud <ngabgnaarnaud2@gmail.com>
2024-09-23 02:27:40 +0200

    Initial commit

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Mapper;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CategoryMapper _categoryMapper;
+        private readonly ICategoryRepository _categoryRepository;
+        
+        public CategoriesController(CategoryMapper categoryMapper,ICategoryRepository categoryRepository)
+        {
+            _categoryMapper = categoryMapper;
+            _categoryRepository = categoryRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = _categoryMapper.RequestCategoryToCategory(request);
+            
+            await _categoryRepository.CreateAsync(category);
+
+            var Response = new
+            {
+                Message = "Message",
+                Response = "Category creer avec success"
+            };
+            return Ok(Response);
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Mapper/CategoryMapper.cs
@@ -0,0 +1,14 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Riok.Mapperly.Abstractions;
+
+namespace CodePulse.API.Mapper
+{
+    [Mapper]
+    public partial class CategoryMapper
+    {
+        public partial CreateCategoryRequestDto CategoryToRequestCategoryDto(Category category);
+        public partial CategoryDto CategoryToCategoryDto(Category category);
+        public partial Category RequestCategoryToCategory(CreateCategoryRequestDto createCategoryRequestDto);
+    }
+}
